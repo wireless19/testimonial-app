88084d4bb4b6374971a95dfb943baf69076911b6
victor <victorifeanyichukwu4@gmail.com>
2021-08-16 13:16:47 +0100

    Added an image Schema and Installed multer for uploading the pictures to the database

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 require('dotenv').config();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + "-" + new Date().toDateString() + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        var ext = path.extname(file.originalname);
+        if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
+            return cb(new Error("only images are allowed"))
+        }
+        cb(null, true)
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 1024
+    }
+});
 
 const app = express();
 
@@ -13,6 +39,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(express.static("public"));
 
 // mongoose.connect("mongodb://localhost:27017/testimonialDB", {
@@ -37,48 +64,77 @@ const clientSchema = new mongoose.Schema({
     stories: {
         type: String,
         required: [true, "Please share your story"]
+    },
+    img: {
+        data: Buffer,
+        contentType: String,
+        // required: true
     }
 });
 
 const Testimony = mongoose.model("Testimony", clientSchema);
 
-const firstPerson = new Testimony({
-    name: "Uzor Mbah",
-    location: "Anambra",
-    stories: "I had the best experience working with VAAL DIGITAL. Usability of the website was great, very good customer service, an all round great experience. I would definitely be coming back!."
-});
 
-const secondPerson = new Testimony({
-    name: "Veronicaa",
-    location: "Anambra",
-    stories: "I had the best experience working with VAAL DIGITAL. Usability of the website was great, very good customer service, an all round great experience. I would definitely be coming back!."
-});
+// inserting documents into the database manually
 
-const thirdPerson = new Testimony({
-    name: "Chidimma Nnaji",
-    location: "Anambra",
-    stories: "I had the best experience working with VAAL DIGITAL. Usability of the website was great, very good customer service, an all round great experience. I would definitely be coming back!."
-});
+// const firstPerson = new Testimony({
+//     name: "Uzor Mbah",
+//     location: "Anambra",
+//     stories: "I had the best experience working with VAAL DIGITAL. Usability of the website was great, very good customer service, an all round great experience. I would definitely be coming back!."
+// });
 
-const defaultPersons = [firstPerson, secondPerson, thirdPerson];
+// const secondPerson = new Testimony({
+//     name: "Veronicaa",
+//     location: "Anambra",
+//     stories: "I had the best experience working with VAAL DIGITAL. Usability of the website was great, very good customer service, an all round great experience. I would definitely be coming back!."
+// });
 
+// const thirdPerson = new Testimony({
+//     name: "Chidimma Nnaji",
+//     location: "Anambra",
+//     stories: "I had the best experience working with VAAL DIGITAL. Usability of the website was great, very good customer service, an all round great experience. I would definitely be coming back!."
+// });
+
+// const defaultPersons = [firstPerson, secondPerson, thirdPerson];
+
+
+// app.get("/", function (req, res) {
+
+//     Testimony.find({}, (err, testimonies) => {
+//         // Check to see if Items collection is empty
+//         if (testimonies.length === 0) {
+//             // If empty, insert default items
+//             Testimony.insertMany(defaultPersons, (err) => {
+//                 if (err) {
+//                     console.log(err);
+//                 } else {
+//                     console.log("Successfully saved default items to DB");
+//                 }
+//             });
+//             res.redirect("/");
+//         } else {
+
+//             res.render("home", {
+//                 testimonies: testimonies,
+//                 smessage: ""
+
+//             });
+//         }
+
+//     });
+
+// });
 
 app.get("/", function (req, res) {
 
     Testimony.find({}, (err, testimonies) => {
-        // Check to see if Items collection is empty
-        if (testimonies.length === 0) {
-            // If empty, insert default items
-            Testimony.insertMany(defaultPersons, (err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("Successfully saved default items to DB");
-                }
-            });
-            res.redirect("/");
-        } else {
 
+        if (err) {
+
+            console.log(err);
+
+        } else {
+            console.log(testimonies);
             res.render("home", {
                 testimonies: testimonies,
                 smessage: ""
@@ -90,34 +146,75 @@ app.get("/", function (req, res) {
 
 });
 
-app.post("/", function (req, res) {
-    const story = new Testimony({
+app.post("/", upload.single("image"), function (req, res, next) {
+
+    // saving to database without pictures
+
+    // const story = new Testimony({
+    //     name: req.body.fname,
+    //     location: req.body.place,
+    //     stories: req.body.story,
+    // });
+
+    // story.save((err) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log("saved successfully");
+    //         Testimony.find({}, (err, testimonies) => {
+
+    //             res.render("home", {
+    //                 testimonies: testimonies,
+    //                 smessage: "Story shared successfully"
+
+    //             });
+
+    //         });
+    //     }
+
+    // });
+
+
+    // saving to database with pictures
+
+    const story = {
         name: req.body.fname,
         location: req.body.place,
-        stories: req.body.story
-    });
+        stories: req.body.story,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + "/uploads/" + req.file.filename)),
+            // contentType: "image/png"
+        }
+    }
 
-    story.save((err) => {
+    Testimony.create(story, function (err, eachtestimony) {
         if (err) {
             console.log(err);
-        } else {
-            console.log("saved successfully");
-            Testimony.find({}, (err, testimonies) => {
+        }
+        else {
+            eachtestimony.save((err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("saved successfully");
+                    Testimony.find({}, (err, testimonies) => {
 
-                res.render("home", {
-                    testimonies: testimonies,
-                    smessage: "Story shared successfully"
+                        res.render("home", {
+                            testimonies: testimonies,
+                            smessage: "Story shared successfully"
 
-                });
+                        });
+
+                    });
+                }
 
             });
         }
-
     });
 
 
 });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 4000, function () {
+    console.log("Server started on port 4000");
 });
